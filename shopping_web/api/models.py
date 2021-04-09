0d3566e1c2b72161b2e9a54e7eb38ec518104d5e
kushaneranga <kem.eranga98@gmail.com>
2021-04-09 09:09:18 +0530

    Django

--- shopping_web/api/models.py
+++ shopping_web/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class TypesCode:
+    id: int
+    name: str
+    oldprice: str
+    newprice: str
+    img: str
+    sale: str
+   
--- shopping_web/api/views.py
+++ shopping_web/api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TypesCode
 
 # Create your views here.
 
 
 def main(request):
-    return render(request, 'index.html')
+
+    typc1 = TypesCode()
+    typc1.name = 'Lorem ipsum dolor'
+    typc1.oldprice = '170,000'
+    typc1.newprice = '150,000'
+    typc1.img = 'fabian-heimann-4R_WEmhx8og-unsplash.jpg'
+
+    return render(request, 'index.html', {'typc1':typc1})
